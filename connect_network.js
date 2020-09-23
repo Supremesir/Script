0dd675b10a41ef20048fc11c6983b05d17d61852
SupremeSir <haofanfang@gmail.com>
2020-09-23 18:03:23 +0800

    Create connect_network.js

---
+++ connect_network.js
@@ -0,0 +1,7 @@
+document.getElementById('username').value='171002315';
+document.getElementById('password').value='4008208820';
+var inputs =  document.getElementsByTagName('input');
+for (var i =0;i<inputs.length;i++) {
+        if  (inputs[i].value == "连接网络")
+            inputs[i].click()
+    }
